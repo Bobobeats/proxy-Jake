fe26943c1f83a3397926c6b1952852eca775a8e3
Jake Wija <jakewija@icloud.com[core]>
2020-02-08 14:59:16 -0800

    deleted 'edited' expresseion

--- server/index.js
+++ server/index.js
@@ -51,45 +51,47 @@ app.get('/api/comments/songs/:songId', (req, res, next) => {
 
 // PLAYER ROUTES
 app.get('/player_bundle', (req, res) => {
-  axios.get(`${playerIp}/player_bundle`)
-  .then(data => {
-    res.send(data.data);
-  })
-  .catch(err => {
-    console.log('ERROR RETRIEVING PLAYER BUNDLE', err);
-    res.send(err);
-  });
-})
+  axios
+    .get(`${playerIp}/player_bundle`)
+    .then((data) => {
+      res.send(data.data);
+    })
+    .catch((err) => {
+      console.log('ERROR RETRIEVING PLAYER BUNDLE', err);
+      res.send(err);
+    });
+});
 app.get('/api/player/songs/:songId', (req, res) => {
   const { songId } = req.params;
-  axios.get(`${playerIp}/api/player/songs/${songId}`)
-  .then(data => res.send(data.data))
-  .catch(err => {
-    console.log('ERROR RETRIEVING PLAYER FOR SONG', err);
-    res.send(err);
-  })
-})
+  axios
+    .get(`${playerIp}/api/player/songs/${songId}`)
+    .then((data) => res.send(data.data))
+    .catch((err) => {
+      console.log('ERROR RETRIEVING PLAYER FOR SONG', err);
+      res.send(err);
+    });
+});
 
 // RELATED ROUTES
 app.get(`/api/relatedTracks/songs/:songId`, (req, res) => {
   const { songId } = req.params;
-  axios.get(`${relatedTracksIp}/api/relatedTracks/songs/${songId}`)
-  .then(data => res.send(data.data))
-  .catch(err => {
-    console.log('ERROR RETRIEVING RELATED TRACKS FOR SONG', err);
-    res.send(err);
-  })
-}) (edited) 
-
-app.get(`/relatedTracks_bundle`, (req, res) => {
-  axios.get(`${relatedTracksIp}/relatedTracks_bundle`)
-  .then(data => res.send(data.data))
-  .catch(err => {
-    console.log('ERROR RETRIEVING RELATED TRACKS BUNDLE', err);
-    res.send(err);
-  })
+  axios
+    .get(`${relatedTracksIp}/api/relatedTracks/songs/${songId}`)
+    .then((data) => res.send(data.data))
+    .catch((err) => {
+      console.log('ERROR RETRIEVING RELATED TRACKS FOR SONG', err);
+      res.send(err);
+    });
 });
 
-
+app.get(`/relatedTracks_bundle`, (req, res) => {
+  axios
+    .get(`${relatedTracksIp}/relatedTracks_bundle`)
+    .then((data) => res.send(data.data))
+    .catch((err) => {
+      console.log('ERROR RETRIEVING RELATED TRACKS BUNDLE', err);
+      res.send(err);
+    });
+});
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}, yo`));
