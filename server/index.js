e7129d03265012d9d3d0bfb01aa07e705d56d204
Jake Wija <jakewija@icloud.com[core]>
2020-02-08 13:28:27 -0800

    CHANGE ports to match computer to docker mapping

--- server/index.js
+++ server/index.js
@@ -1,54 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const axios = require('axios')
-
+const axios = require('axios');
 
 const commentsIP = `http://35.163.180.83:4001`;
 const playerIp = `http://44.232.36.205:3002`;
 const relatedTracksIp = `http://44.226.147.90:1000`;
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
-console.log('getting')
+console.log('getting');
 
 // COMMENTS ROUTES
 app.get('/comments_bundle', (req, res, next) => {
-  console.log('hello')
-  axios.get(commentsIP + '/comments_bundle')
+  console.log('hello');
+  axios
+    .get(commentsIP + '/comments_bundle')
     .then((response) => {
-      const data = response.data
+      const data = response.data;
       console.log('got something');
       res.send(data);
     })
     .catch((err) => {
       console.log("didn't get something");
-      console.log(err)
-      res.send(err)
-    })
+      console.log(err);
+      res.send(err);
+    });
 });
 
 app.get('/api/comments/songs/:songId', (req, res, next) => {
   let { page, limit, join } = req.query;
   const { songId } = req.params;
   limit = Number(limit);
-  console.log('proxy asking for json')
-  axios.get(`${commentsIP}/api/comments/songs/${songId}?page=${page}&limit=${limit}&join=false`)
+  console.log('proxy asking for json');
+  axios
+    .get(
+      `${commentsIP}/api/comments/songs/${songId}?page=${page}&limit=${limit}&join=false`,
+    )
     .then((response) => {
       const data = response.data;
       console.log(data);
       res.send(data);
     })
     .catch((err) => {
-      console.log(err)
-    })
+      console.log(err);
+    });
 });
-
 
 // // PLAYER ROUTES
 // app.get('/player_bundle', (req, res) => {
@@ -90,6 +92,5 @@ app.get('/api/comments/songs/:songId', (req, res, next) => {
 //     res.send(err);
 //   })
 // })
-
 
 app.listen(8080, () => console.log(`listening to port ${PORT}, yo`));
